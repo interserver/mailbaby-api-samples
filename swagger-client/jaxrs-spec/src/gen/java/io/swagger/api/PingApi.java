630864b7a76062020f1f2fd3c799bd40d2137dc9
Joe Huss <detain@interserver.net>
2021-04-28 17:21:18 -0400

    adding swagger client jaxrs-spec

---
+++ swagger-client/jaxrs-spec/src/gen/java/io/swagger/api/PingApi.java
@@ -0,0 +1,34 @@
+package io.swagger.api;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+import java.util.Map;
+import java.util.List;
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+@Path("/ping")
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2021-04-28T17:21:16.928072-04:00[America/New_York]")
+public class PingApi {
+
+    @GET
+    @Operation(summary = "Checks if the server is running", description = "", tags={  })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Server is up and running"),
+        @ApiResponse(responseCode = "200", description = "Something is wrong")
+    })
+    public Response pingServer() {
+        return Response.ok().entity("magic!").build();
+    }}

9f9fe3b54eaa9334a2fdd0ecc7d491283b0ca053
Joe Huss <detain@interserver.net>
2021-04-28 17:14:19 -0400

    Adding openapi client dart-dio-next

---
+++ openapi-client/dart-dio-next/lib/src/api/default_api.dart
@@ -0,0 +1,680 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+
+import 'dart:async';
+
+import 'package:built_value/serializer.dart';
+import 'package:dio/dio.dart';
+
+import 'package:openapi/src/model/generic_response.dart';
+import 'package:openapi/src/model/mail_log.dart';
+import 'package:openapi/src/model/error_response.dart';
+import 'package:openapi/src/model/send_mail.dart';
+import 'package:openapi/src/model/mail_order.dart';
+import 'package:built_collection/built_collection.dart';
+
+class DefaultApi {
+
+  final Dio _dio;
+
+  final Serializers _serializers;
+
+  const DefaultApi(this._dio, this._serializers);
+
+  /// Gets mail order information by id
+  ///
+  /// returns information about a mail order in the system with the given id.
+  Future<Response<MailOrder>> getMailById({ 
+    required int id,
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/mail/{id}'.replaceAll('{' r'id' '}', id.toString());
+    final _options = Options(
+      method: r'GET',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[
+          {
+            'type': 'apiKey',
+            'name': 'apiKeyAuth',
+            'keyName': 'X-API-KEY',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiLoginAuth',
+            'keyName': 'X-API-LOGIN',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiPasswordAuth',
+            'keyName': 'X-API-PASS',
+            'where': 'header',
+          },
+        ],
+        ...?extra,
+      },
+      contentType: [
+        'application/json',
+      ].first,
+      validateStatus: validateStatus,
+    );
+
+    final _queryParameters = <String, dynamic>{
+    };
+
+    final _response = await _dio.request<Object>(
+      _path,
+      options: _options,
+      queryParameters: _queryParameters,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    MailOrder _responseData;
+
+    try {
+      const _responseType = FullType(MailOrder);
+      _responseData = _serializers.deserialize(
+        _response.data!,
+        specifiedType: _responseType,
+      ) as MailOrder;
+
+    } catch (error) {
+      throw DioError(
+        requestOptions: _response.requestOptions,
+        response: _response,
+        type: DioErrorType.other,
+        error: error,
+      );
+    }
+
+    return Response<MailOrder>(
+      data: _responseData,
+      headers: _response.headers,
+      isRedirect: _response.isRedirect,
+      requestOptions: _response.requestOptions,
+      redirects: _response.redirects,
+      statusCode: _response.statusCode,
+      statusMessage: _response.statusMessage,
+      extra: _response.extra,
+    );
+  }
+
+  /// displays a list of mail service orders
+  ///
+  /// 
+  Future<Response<BuiltList<MailOrder>>> getMailOrders({ 
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/mail';
+    final _options = Options(
+      method: r'GET',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[
+          {
+            'type': 'apiKey',
+            'name': 'apiKeyAuth',
+            'keyName': 'X-API-KEY',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiLoginAuth',
+            'keyName': 'X-API-LOGIN',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiPasswordAuth',
+            'keyName': 'X-API-PASS',
+            'where': 'header',
+          },
+        ],
+        ...?extra,
+      },
+      contentType: [
+        'application/json',
+      ].first,
+      validateStatus: validateStatus,
+    );
+
+    final _queryParameters = <String, dynamic>{
+    };
+
+    final _response = await _dio.request<Object>(
+      _path,
+      options: _options,
+      queryParameters: _queryParameters,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    BuiltList<MailOrder> _responseData;
+
+    try {
+      const _responseType = FullType(BuiltList, [FullType(MailOrder)]);
+      _responseData = _serializers.deserialize(
+        _response.data!,
+        specifiedType: _responseType,
+      ) as BuiltList<MailOrder>;
+
+    } catch (error) {
+      throw DioError(
+        requestOptions: _response.requestOptions,
+        response: _response,
+        type: DioErrorType.other,
+        error: error,
+      );
+    }
+
+    return Response<BuiltList<MailOrder>>(
+      data: _responseData,
+      headers: _response.headers,
+      isRedirect: _response.isRedirect,
+      requestOptions: _response.requestOptions,
+      redirects: _response.redirects,
+      statusCode: _response.statusCode,
+      statusMessage: _response.statusMessage,
+      extra: _response.extra,
+    );
+  }
+
+  /// Checks if the server is running
+  ///
+  /// 
+  Future<Response<void>> pingServer({ 
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/ping';
+    final _options = Options(
+      method: r'GET',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[],
+        ...?extra,
+      },
+      contentType: [
+        'application/json',
+      ].first,
+      validateStatus: validateStatus,
+    );
+
+    final _queryParameters = <String, dynamic>{
+    };
+
+    final _response = await _dio.request<Object>(
+      _path,
+      options: _options,
+      queryParameters: _queryParameters,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    return _response;
+  }
+
+  /// places a mail order
+  ///
+  /// Adds an item to the system
+  Future<Response<void>> placeMailOrder({ 
+    MailOrder? mailOrder,
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/mail/order';
+    final _options = Options(
+      method: r'POST',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[
+          {
+            'type': 'apiKey',
+            'name': 'apiKeyAuth',
+            'keyName': 'X-API-KEY',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiLoginAuth',
+            'keyName': 'X-API-LOGIN',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiPasswordAuth',
+            'keyName': 'X-API-PASS',
+            'where': 'header',
+          },
+        ],
+        ...?extra,
+      },
+      contentType: [
+        'application/json',
+      ].first,
+      validateStatus: validateStatus,
+    );
+
+    final _queryParameters = <String, dynamic>{
+    };
+
+    dynamic _bodyData;
+
+    try {
+      const _type = FullType(MailOrder);
+      _bodyData = mailOrder == null ? null : _serializers.serialize(mailOrder, specifiedType: _type);
+
+    } catch(error) {
+      throw DioError(
+         requestOptions: _options.compose(
+          _dio.options,
+          _path,
+          queryParameters: _queryParameters,
+        ),
+        type: DioErrorType.other,
+        error: error,
+      );
+    }
+
+    final _response = await _dio.request<Object>(
+      _path,
+      data: _bodyData,
+      options: _options,
+      queryParameters: _queryParameters,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    return _response;
+  }
+
+  /// Sends an Email with Advanced Options
+  ///
+  /// Sends An email through one of your mail orders allowing additional options such as file attachments, cc, bcc, etc.
+  Future<Response<GenericResponse>> sendAdvMailById({ 
+    required int id,
+    required SendMail sendMail,
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/mail/{id}/advsend'.replaceAll('{' r'id' '}', id.toString());
+    final _options = Options(
+      method: r'POST',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[
+          {
+            'type': 'apiKey',
+            'name': 'apiKeyAuth',
+            'keyName': 'X-API-KEY',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiLoginAuth',
+            'keyName': 'X-API-LOGIN',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiPasswordAuth',
+            'keyName': 'X-API-PASS',
+            'where': 'header',
+          },
+        ],
+        ...?extra,
+      },
+      contentType: [
+        'application/json',
+        'application/xml',
+        'application/x-www-form-urlencoded',
+        'text/plain',
+      ].first,
+      validateStatus: validateStatus,
+    );
+
+    final _queryParameters = <String, dynamic>{
+    };
+
+    dynamic _bodyData;
+
+    try {
+      const _type = FullType(SendMail);
+      _bodyData = _serializers.serialize(sendMail, specifiedType: _type);
+
+    } catch(error) {
+      throw DioError(
+         requestOptions: _options.compose(
+          _dio.options,
+          _path,
+          queryParameters: _queryParameters,
+        ),
+        type: DioErrorType.other,
+        error: error,
+      );
+    }
+
+    final _response = await _dio.request<Object>(
+      _path,
+      data: _bodyData,
+      options: _options,
+      queryParameters: _queryParameters,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    GenericResponse _responseData;
+
+    try {
+      const _responseType = FullType(GenericResponse);
+      _responseData = _serializers.deserialize(
+        _response.data!,
+        specifiedType: _responseType,
+      ) as GenericResponse;
+
+    } catch (error) {
+      throw DioError(
+        requestOptions: _response.requestOptions,
+        response: _response,
+        type: DioErrorType.other,
+        error: error,
+      );
+    }
+
+    return Response<GenericResponse>(
+      data: _responseData,
+      headers: _response.headers,
+      isRedirect: _response.isRedirect,
+      requestOptions: _response.requestOptions,
+      redirects: _response.redirects,
+      statusCode: _response.statusCode,
+      statusMessage: _response.statusMessage,
+      extra: _response.extra,
+    );
+  }
+
+  /// Sends an Email
+  ///
+  /// Sends An email through one of your mail orders.
+  Future<Response<GenericResponse>> sendMailById({ 
+    required int id,
+    String? subject,
+    String? body,
+    String? to,
+    String? toName,
+    String? from,
+    String? fromName,
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/mail/{id}/send'.replaceAll('{' r'id' '}', id.toString());
+    final _options = Options(
+      method: r'POST',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[
+          {
+            'type': 'apiKey',
+            'name': 'apiKeyAuth',
+            'keyName': 'X-API-KEY',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiLoginAuth',
+            'keyName': 'X-API-LOGIN',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiPasswordAuth',
+            'keyName': 'X-API-PASS',
+            'where': 'header',
+          },
+        ],
+        ...?extra,
+      },
+      contentType: [
+        'application/json',
+      ].first,
+      validateStatus: validateStatus,
+    );
+
+    final _queryParameters = <String, dynamic>{
+      if (subject != null) r'subject': subject,
+      if (body != null) r'body': body,
+      if (to != null) r'to': to,
+      if (toName != null) r'toName': toName,
+      if (from != null) r'from': from,
+      if (fromName != null) r'fromName': fromName,
+    };
+
+    final _response = await _dio.request<Object>(
+      _path,
+      options: _options,
+      queryParameters: _queryParameters,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    GenericResponse _responseData;
+
+    try {
+      const _responseType = FullType(GenericResponse);
+      _responseData = _serializers.deserialize(
+        _response.data!,
+        specifiedType: _responseType,
+      ) as GenericResponse;
+
+    } catch (error) {
+      throw DioError(
+        requestOptions: _response.requestOptions,
+        response: _response,
+        type: DioErrorType.other,
+        error: error,
+      );
+    }
+
+    return Response<GenericResponse>(
+      data: _responseData,
+      headers: _response.headers,
+      isRedirect: _response.isRedirect,
+      requestOptions: _response.requestOptions,
+      redirects: _response.redirects,
+      statusCode: _response.statusCode,
+      statusMessage: _response.statusMessage,
+      extra: _response.extra,
+    );
+  }
+
+  /// validatess order details before placing an order
+  ///
+  /// 
+  Future<Response<void>> validateMailOrder({ 
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/mail/order';
+    final _options = Options(
+      method: r'GET',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[
+          {
+            'type': 'apiKey',
+            'name': 'apiKeyAuth',
+            'keyName': 'X-API-KEY',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiLoginAuth',
+            'keyName': 'X-API-LOGIN',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiPasswordAuth',
+            'keyName': 'X-API-PASS',
+            'where': 'header',
+          },
+        ],
+        ...?extra,
+      },
+      contentType: [
+        'application/json',
+      ].first,
+      validateStatus: validateStatus,
+    );
+
+    final _queryParameters = <String, dynamic>{
+    };
+
+    final _response = await _dio.request<Object>(
+      _path,
+      options: _options,
+      queryParameters: _queryParameters,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    return _response;
+  }
+
+  /// displays the mail log
+  ///
+  /// By passing in the appropriate options, you can search for available inventory in the system 
+  Future<Response<BuiltList<MailLog>>> viewMailLogById({ 
+    required int id,
+    String? searchString,
+    int? skip,
+    int? limit,
+    CancelToken? cancelToken,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? extra,
+    ValidateStatus? validateStatus,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    final _path = r'/mail/{id}/log'.replaceAll('{' r'id' '}', id.toString());
+    final _options = Options(
+      method: r'GET',
+      headers: <String, dynamic>{
+        ...?headers,
+      },
+      extra: <String, dynamic>{
+        'secure': <Map<String, String>>[
+          {
+            'type': 'apiKey',
+            'name': 'apiKeyAuth',
+            'keyName': 'X-API-KEY',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiLoginAuth',
+            'keyName': 'X-API-LOGIN',
+            'where': 'header',
+          },{
+            'type': 'apiKey',
+            'name': 'apiPasswordAuth',
+            'keyName': 'X-API-PASS',
+            'where': 'header',
+          },
+        ],
+        ...?extra,
+      },
+      contentType: [
+        'application/json',
+      ].first,
+      validateStatus: validateStatus,
+    );
+
+    final _queryParameters = <String, dynamic>{
+      if (searchString != null) r'searchString': searchString,
+      if (skip != null) r'skip': skip,
+      if (limit != null) r'limit': limit,
+    };
+
+    final _response = await _dio.request<Object>(
+      _path,
+      options: _options,
+      queryParameters: _queryParameters,
+      cancelToken: cancelToken,
+      onSendProgress: onSendProgress,
+      onReceiveProgress: onReceiveProgress,
+    );
+
+    BuiltList<MailLog> _responseData;
+
+    try {
+      const _responseType = FullType(BuiltList, [FullType(MailLog)]);
+      _responseData = _serializers.deserialize(
+        _response.data!,
+        specifiedType: _responseType,
+      ) as BuiltList<MailLog>;
+
+    } catch (error) {
+      throw DioError(
+        requestOptions: _response.requestOptions,
+        response: _response,
+        type: DioErrorType.other,
+        error: error,
+      );
+    }
+
+    return Response<BuiltList<MailLog>>(
+      data: _responseData,
+      headers: _response.headers,
+      isRedirect: _response.isRedirect,
+      requestOptions: _response.requestOptions,
+      redirects: _response.redirects,
+      statusCode: _response.statusCode,
+      statusMessage: _response.statusMessage,
+      extra: _response.extra,
+    );
+  }
+
+}

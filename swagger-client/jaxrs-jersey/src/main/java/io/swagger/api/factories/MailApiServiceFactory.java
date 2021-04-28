90990c062240917a49fd5dcfae2159c91863331c
Joe Huss <detain@interserver.net>
2021-04-28 17:21:21 -0400

    adding swagger client jaxrs-jersey

---
+++ swagger-client/jaxrs-jersey/src/main/java/io/swagger/api/factories/MailApiServiceFactory.java
@@ -0,0 +1,12 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.MailApiService;
+import io.swagger.api.impl.MailApiServiceImpl;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2021-04-28T17:21:19.938868-04:00[America/New_York]")public class MailApiServiceFactory {
+    private final static MailApiService service = new MailApiServiceImpl();
+
+    public static MailApiService getMailApi() {
+        return service;
+    }
+}
---
+++ swagger-client/jaxrs-jersey/src/main/java/io/swagger/api/factories/PingApiServiceFactory.java
@@ -0,0 +1,12 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.PingApiService;
+import io.swagger.api.impl.PingApiServiceImpl;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2021-04-28T17:21:19.938868-04:00[America/New_York]")public class PingApiServiceFactory {
+    private final static PingApiService service = new PingApiServiceImpl();
+
+    public static PingApiService getPingApi() {
+        return service;
+    }
+}

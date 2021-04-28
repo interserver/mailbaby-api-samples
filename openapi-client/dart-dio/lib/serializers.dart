d92d5dc3504673f3f08400f78015d41de5374542
Joe Huss <detain@interserver.net>
2021-04-28 17:17:21 -0400

    Adding openapi client dart-dio

---
+++ openapi-client/dart-dio/lib/serializers.dart
@@ -0,0 +1,48 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.7
+
+// ignore_for_file: unused_import
+
+library serializers;
+
+import 'package:built_value/iso_8601_date_time_serializer.dart';
+import 'package:built_value/serializer.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/json_object.dart';
+import 'package:built_value/standard_json_plugin.dart';
+
+import 'package:openapi/model/error_response.dart';
+import 'package:openapi/model/generic_response.dart';
+import 'package:openapi/model/mail_attachment.dart';
+import 'package:openapi/model/mail_contact.dart';
+import 'package:openapi/model/mail_log.dart';
+import 'package:openapi/model/mail_order.dart';
+import 'package:openapi/model/send_mail.dart';
+
+part 'serializers.g.dart';
+
+@SerializersFor(const [
+  ErrorResponse,
+  GenericResponse,
+  MailAttachment,
+  MailContact,
+  MailLog,
+  MailOrder,
+  SendMail,
+])
+Serializers serializers = (_$serializers.toBuilder()
+      ..addBuilderFactory(
+        const FullType(BuiltList, [FullType(MailLog)]),
+        () => ListBuilder<MailLog>(),
+      )
+      ..addBuilderFactory(
+        const FullType(BuiltList, [FullType(MailOrder)]),
+        () => ListBuilder<MailOrder>(),
+      )
+      ..add(Iso8601DateTimeSerializer()))
+    .build();
+
+Serializers standardSerializers =
+    (serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
---
+++ openapi-client/dart-dio/test/mail_contact_test.dart
@@ -0,0 +1,32 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.7
+
+// ignore_for_file: unused_import
+
+import 'package:openapi/model/mail_contact.dart';
+import 'package:test/test.dart';
+
+// tests for MailContact
+void main() {
+  final instance = MailContactBuilder();
+  // TODO add properties to the builder and call build()
+
+  group(MailContact, () {
+    // The email address
+    // String email
+    test('to test the property `email`', () async {
+      // TODO
+    });
+
+    // Optional contact name
+    // String name
+    test('to test the property `name`', () async {
+      // TODO
+    });
+
+
+  });
+
+}

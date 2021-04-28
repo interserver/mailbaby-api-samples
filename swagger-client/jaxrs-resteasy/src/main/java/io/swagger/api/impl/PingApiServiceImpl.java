85679b4295568d8db4135bb4c6a3aab2c8b1b260
Joe Huss <detain@interserver.net>
2021-04-28 17:21:31 -0400

    adding swagger client jaxrs-resteasy

---
+++ swagger-client/jaxrs-resteasy/src/main/java/io/swagger/api/impl/PingApiServiceImpl.java
@@ -0,0 +1,23 @@
+package io.swagger.api.impl;
+
+import io.swagger.api.*;
+import io.swagger.model.*;
+
+
+import java.util.List;
+import io.swagger.api.NotFoundException;
+
+import java.io.InputStream;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@RequestScoped
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaResteasyServerCodegen", date = "2021-04-28T17:21:29.942742-04:00[America/New_York]")public class PingApiServiceImpl implements PingApiService {
+      public Response pingServer(SecurityContext securityContext)
+      throws NotFoundException {
+      // do some magic!
+      return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+  }
+}

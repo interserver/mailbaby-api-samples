f52b6d4bf57f40c73b3c28672b9bc7bdbb78aa43
Joe Huss <detain@interserver.net>
2021-04-28 17:21:28 -0400

    Removing swagger client jaxrs-resteasy

--- swagger-client/jaxrs-resteasy/src/gen/java/io/swagger/api/ApiException.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.api;
-
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaResteasyServerCodegen", date = "2021-04-28T16:58:46.388835-04:00[America/New_York]")public class ApiException extends Exception{
-    private int code;
-    public ApiException (int code, String msg) {
-        super(msg);
-        this.code = code;
-    }
-}

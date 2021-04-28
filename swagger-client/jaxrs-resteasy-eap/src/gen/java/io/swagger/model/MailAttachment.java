3678a4792b00c67b41757c046ef65058a67b701f
Joe Huss <detain@interserver.net>
2021-04-28 17:21:28 -0400

    adding swagger client jaxrs-resteasy-eap

---
+++ swagger-client/jaxrs-resteasy-eap/src/gen/java/io/swagger/model/MailAttachment.java
@@ -0,0 +1,84 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.io.File;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description="A File attachment for an email")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaResteasyEapServerCodegen", date = "2021-04-28T17:21:26.533991-04:00[America/New_York]")public class MailAttachment   {
+  private String filename = null;
+  private File data = null;
+
+  /**
+   * Optional filename to specify for the attachment.
+   **/
+  
+  @Schema(example = "company_logo.png", description = "Optional filename to specify for the attachment.")
+  @JsonProperty("filename")
+  public String getFilename() {
+    return filename;
+  }
+  public void setFilename(String filename) {
+    this.filename = filename;
+  }
+
+  /**
+   * Contents of the attached file
+   **/
+  
+  @Schema(required = true, description = "Contents of the attached file")
+  @JsonProperty("data")
+  @NotNull
+  public File getData() {
+    return data;
+  }
+  public void setData(File data) {
+    this.data = data;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MailAttachment mailAttachment = (MailAttachment) o;
+    return Objects.equals(filename, mailAttachment.filename) &&
+        Objects.equals(data, mailAttachment.data);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(filename, data);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class MailAttachment {\n");
+    
+    sb.append("    filename: ").append(toIndentedString(filename)).append("\n");
+    sb.append("    data: ").append(toIndentedString(data)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ swagger-client/jaxrs-resteasy-eap/src/main/java/io/swagger/api/impl/MailApiServiceImpl.java
@@ -0,0 +1,57 @@
+package io.swagger.api.impl;
+
+import io.swagger.api.*;
+import io.swagger.model.*;
+
+import io.swagger.model.ErrorResponse;
+import io.swagger.model.GenericResponse;
+import io.swagger.model.MailLog;
+import io.swagger.model.MailOrder;
+import io.swagger.model.MailOrders;
+import io.swagger.model.SendMail;
+
+import java.util.List;
+
+import java.io.InputStream;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaResteasyEapServerCodegen", date = "2021-04-28T17:21:26.533991-04:00[America/New_York]")public class MailApiServiceImpl implements MailApi {
+      public Response getMailById(Long id,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response getMailOrders(SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response placeMailOrder(MailOrder body,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response sendAdvMailById(SendMail body,Long id,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response sendAdvMailById(Long id2,Long id,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response sendAdvMailById(SendMail body,Long id,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response sendMailById(Long id,String subject,String body,String to,String toName,String from,String fromName,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response validateMailOrder(SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+      public Response viewMailLogById(Long id,String searchString,Integer skip,Integer limit,SecurityContext securityContext) {
+      // do some magic!
+      return Response.ok().build();
+  }
+}

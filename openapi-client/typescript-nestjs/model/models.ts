b3a740f392a45eb0c62eb316cc511f4afe761c48
Joe Huss <detain@interserver.net>
2021-04-28 16:57:08 -0400

    Removing

--- openapi-client/typescript-nestjs/model/models.ts
+++
@@ -1,7 +0,0 @@
-export * from './errorResponse';
-export * from './genericResponse';
-export * from './mailAttachment';
-export * from './mailContact';
-export * from './mailLog';
-export * from './mailOrder';
-export * from './sendMail';

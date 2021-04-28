84d7fabe2fbdf83a7261ed99f494d1fe5cebef9b
Joe Huss <detain@interserver.net>
2021-04-28 17:19:29 -0400

    Adding openapi client typescript-angular

---
+++ openapi-client/typescript-angular/model/models.ts
@@ -0,0 +1,7 @@
+export * from './errorResponse';
+export * from './genericResponse';
+export * from './mailAttachment';
+export * from './mailContact';
+export * from './mailLog';
+export * from './mailOrder';
+export * from './sendMail';

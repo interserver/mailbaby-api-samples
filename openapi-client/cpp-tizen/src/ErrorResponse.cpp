8b81d88797667a50730d34f8e18450b4e93d7057
Joe Huss <detain@interserver.net>
2021-04-28 17:17:01 -0400

    Adding openapi client cpp-tizen

---
+++ openapi-client/cpp-tizen/src/ErrorResponse.cpp
@@ -0,0 +1,135 @@
+#include <map>
+#include <cstdlib>
+#include <glib-object.h>
+#include <json-glib/json-glib.h>
+#include "Helpers.h"
+
+
+#include "ErrorResponse.h"
+
+using namespace std;
+using namespace Tizen::ArtikCloud;
+
+ErrorResponse::ErrorResponse()
+{
+	//__init();
+}
+
+ErrorResponse::~ErrorResponse()
+{
+	//__cleanup();
+}
+
+void
+ErrorResponse::__init()
+{
+	//code = std::string();
+	//message = std::string();
+}
+
+void
+ErrorResponse::__cleanup()
+{
+	//if(code != NULL) {
+	//
+	//delete code;
+	//code = NULL;
+	//}
+	//if(message != NULL) {
+	//
+	//delete message;
+	//message = NULL;
+	//}
+	//
+}
+
+void
+ErrorResponse::fromJson(char* jsonStr)
+{
+	JsonObject *pJsonObject = json_node_get_object(json_from_string(jsonStr,NULL));
+	JsonNode *node;
+	const gchar *codeKey = "code";
+	node = json_object_get_member(pJsonObject, codeKey);
+	if (node !=NULL) {
+	
+
+		if (isprimitive("std::string")) {
+			jsonToValue(&code, node, "std::string", "");
+		} else {
+			
+		}
+	}
+	const gchar *messageKey = "message";
+	node = json_object_get_member(pJsonObject, messageKey);
+	if (node !=NULL) {
+	
+
+		if (isprimitive("std::string")) {
+			jsonToValue(&message, node, "std::string", "");
+		} else {
+			
+		}
+	}
+}
+
+ErrorResponse::ErrorResponse(char* json)
+{
+	this->fromJson(json);
+}
+
+char*
+ErrorResponse::toJson()
+{
+	JsonObject *pJsonObject = json_object_new();
+	JsonNode *node;
+	if (isprimitive("std::string")) {
+		std::string obj = getCode();
+		node = converttoJson(&obj, "std::string", "");
+	}
+	else {
+		
+	}
+	const gchar *codeKey = "code";
+	json_object_set_member(pJsonObject, codeKey, node);
+	if (isprimitive("std::string")) {
+		std::string obj = getMessage();
+		node = converttoJson(&obj, "std::string", "");
+	}
+	else {
+		
+	}
+	const gchar *messageKey = "message";
+	json_object_set_member(pJsonObject, messageKey, node);
+	node = json_node_alloc();
+	json_node_init(node, JSON_NODE_OBJECT);
+	json_node_take_object(node, pJsonObject);
+	char * ret = json_to_string(node, false);
+	json_node_free(node);
+	return ret;
+}
+
+std::string
+ErrorResponse::getCode()
+{
+	return code;
+}
+
+void
+ErrorResponse::setCode(std::string  code)
+{
+	this->code = code;
+}
+
+std::string
+ErrorResponse::getMessage()
+{
+	return message;
+}
+
+void
+ErrorResponse::setMessage(std::string  message)
+{
+	this->message = message;
+}
+
+

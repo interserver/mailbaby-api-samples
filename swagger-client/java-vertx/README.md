51562b26cbe3320bfebaa2ff21be43971d09dda7
Joe Huss <detain@interserver.net>
2023-08-14 20:02:48 -0400

    updates

--- swagger-client/java-vertx/README.md
+++
@@ -1,1 +0,0 @@
-Project generated on : 2023-08-10T12:38:17.936627-04:00[America/New_York]
--- swagger-client/java-vertx/src/main/java/io/swagger/server/api/service/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(name = "service", groupPackage = "io.swagger.server.api.service", useFutures = false)
-package io.swagger.server.api.service;
-
-import io.vertx.codegen.annotations.ModuleGen;
